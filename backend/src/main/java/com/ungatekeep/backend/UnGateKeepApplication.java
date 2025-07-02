9162d202e4cc3df49dc7959fa9bedaea9ef3283c
arrana16 <69128309+arrana16@users.noreply.github.com>
2025-07-02 15:23:09 +0300

    Initial commit

---
+++ backend/src/main/java/com/ungatekeep/backend/UnGateKeepApplication.java
@@ -0,0 +1,13 @@
+package com.ungatekeep.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnGateKeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnGateKeepApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ungatekeep/backend/UnGateKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ungatekeep.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnGateKeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
